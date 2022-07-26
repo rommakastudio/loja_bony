e69e095d254c03da415c66e14c9c159ea8825cc2
rommaka.studio <rommaka.studio@gmail.com>
2022-07-26 18:51:19 -0300

    Pagination  php mod 36 aula 11

--- views/home.php
+++ views/home.php
@@ -16,3 +16,8 @@ $a = 0;
 	?>
 <?php endforeach; ?>
 </div>
+<div class="paginationArea">
+	<?php for($q=1;$q<=$numberOfPages;$q++): ?>
+		<div class="paginationItem <?php echo ($currentPage==$q)?'pag_active':''; ?> "><a href="<?php echo BASE_URL; ?>?p=<?php echo $q; ?>"><?php echo $q; ?></a></div>
+	<?php endfor; ?>
+</div>
